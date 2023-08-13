ed90d89a9c2463ad268247049694c710ddcc5328
Станислав <stanislavskuratovich37@gmail.com>
2023-08-13 20:07:54 +0300

    Last changes to share project were completed

--- todo_list/todo_list/settings.py
+++ todo_list/todo_list/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!q83xci6riksi*nc__)%4kkd4-+x!4(kkq^mdu4%gqwvg+4(0b'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -133,3 +133,5 @@ STATIC_URL = '/static/'
 INTERNAL_IPS = [
     '127.0.0.1',
 ]
+
+LOGIN_URL = 'login'
